7941ec990f65ccb882b2adff8b3b71e18c28933e
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-08 13:07:23 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -5,12 +5,15 @@ class Project
 
   def initialize(title)
     @title = title
-    @backers = []
+    @backers = [] #  [#<Backer:0x0000000001619198 @backed_projects=
+    #  [#<Project:0x00000000016190f8 @backers=[...],
+    #  @title="Magic The Gathering Thing">],
+  @name="Spencer">]
   end
 
   def add_backer(backer)
     @backers << backer
-    #backer.back_project(self)
-    binding.pry
+    backer.back_project(self)
+
   end
 end
